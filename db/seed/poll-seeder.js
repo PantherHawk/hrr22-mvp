696fda2df88586cb301e09a9c41849dbccad60e2
Alex Rosenthal <alexrosenthalster@gmail.com>
2017-03-13 12:37:34 -0700

    added server

---
+++ db/seed/poll-seeder.js
@@ -0,0 +1,51 @@
+var Poll = require('../models/poll');
+var Q = require('q');
+
+var polls = [
+
+   new Poll ({
+    title: 'Bacon?',
+    question: 'I saw my friend eating bacon and it smelled delicious. Is bacon kosher?!!!',
+    author: 'Mikey Schwartz',
+    upvote: 0,
+    downvote: 0
+  }),
+
+  new Poll ({
+    title: 'Bacon?',
+    question: 'I saw my friend eating bacon and it smelled delicious. Is bacon kosher?!!!',
+    author: 'Mikey Schwartz',
+    upvote: 0,
+    downvote: 0
+  }),
+
+  new Poll ({
+    title: 'Bacon?',
+    question: 'I saw my friend eating bacon and it smelled delicious. Is bacon kosher?!!!',
+    author: 'Mikey Schwartz',
+    upvote: 0,
+    downvote: 0
+  }),
+
+  new Poll ({
+    title: 'Bacon?',
+    question: 'I saw my friend eating bacon and it smelled delicious. Is bacon kosher?!!!',
+    author: 'Mikey Schwartz',
+    upvote: 0,
+    downvote: 0
+  })
+  ];
+
+  var done = 0;
+  for (var i = 0; i < polls.length; i++) {
+    polls[i].save(function(err, result) {
+      done++;
+      if (done === polls.length) {
+        exit();
+      }
+    });
+  }
+
+  function exit() {
+    mongoose.disconnect();
+  }
